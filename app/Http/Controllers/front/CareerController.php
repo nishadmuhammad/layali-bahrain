b3aafd12b22604c13794578409491c1b29997b72
Priyanka <priyanka.op2010@gmail.com>
2020-07-21 21:58:03 +0530

    application form
    Took 1 hour 31 minutes

--- app/Http/Controllers/front/CareerController.php
+++ app/Http/Controllers/front/CareerController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\front;
+use App\Application;
 use App\Http\Controllers\Controller;
 use App\Opening;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class CareerController extends Controller
 {
@@ -14,5 +17,32 @@ class CareerController extends Controller
         $opening=Opening::findOrFail($id);
         $openings=Opening::all();
         return view('front.career.show',['opening'=>$opening,'openings'=>$openings]);
+    }
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+           'name'=>'required',
+           'opening_id'=>'required',
+           'email'=>'required|email',
+            'phone'=>'required',
+            'biodata'=>'required',
+            'description'=>'required',
+        ]);
+        //Uploading and saving outer image
+        if($request->biodata) {
+
+             $filenameWithExt=$request->file('biodata')->getClientOriginalName();
+            $filename=pathinfo($filenameWithExt,PATHINFO_FILENAME);
+            $extension=$request->file('biodata')->getClientOriginalExtension();
+            $fileNameToStore=$filename.'_'.time().'.'.$extension;
+            request()->biodata->move(public_path('/uploads/biodata'), $fileNameToStore);
+            $file_path='uploads/biodata/'.$fileNameToStore;
+            $data['biodata']=$file_path;
+        }
+        else{
+            $data['biodata']='';
+        }
+        Application::create($data);
+        return back()->with('success','Application Submitted Successfully');
     }
 }
