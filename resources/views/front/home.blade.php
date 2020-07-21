249cc84427f40ad584f6dd9e5bf658d1e5f2234f
jithin <parakkajithin@gmail.com>
2020-07-21 23:20:08 +0530

    OG Image added for home page
    Took 6 minutes

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Maple Tech Space')
 @section('description','We are a team of experienced Digital Creators who provide cost effective, contemporary technological solutions for a wider global reach. We help our clients harness the Digital wave by providing exceptional services in Digital Marketing,Website and App Development , Branding, Search Engine Optimisation, IT Consulting, & Software Development')
 @section('keywords','')
-@section('ogImage','')
+@section('ogImage'){{asset('assets/img/logo.png')}}@endsection
 @section('navClass','index')
 @section('logo'){{asset('assets/img/logo.png')}}@endsection
 @section('content')
