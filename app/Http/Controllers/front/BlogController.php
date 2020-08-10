c4f376d3f1e961a3c3d972a66eec6d4251e3e9b1
jithin <parakkajithin@gmail.com>
2020-08-10 16:45:48 +0530

    Status published
    Took 20 minutes

--- app/Http/Controllers/front/BlogController.php
+++ app/Http/Controllers/front/BlogController.php
@@ -7,12 +7,12 @@ use App\Post;
 class BlogController extends Controller
 {
     public function index(){
-        $blogs=Post::OrderBy('id', 'DESC')->get();
+        $blogs=Post::where('status','Published')->OrderBy('id', 'DESC')->get();
         return view('front.blog.index',['blogs'=>$blogs]);
     }
     public function show($slug){
         $blog=Post::where('slug',$slug)->firstOrFail();
-        $blogs=Post::where('slug','<>',$slug)->OrderBy('id', 'DESC')->limit(4)->get();
+        $blogs=Post::where([['slug','<>',$slug].['status','Published']])->OrderBy('id', 'DESC')->limit(4)->get();
         return view('front.blog.show',['blog'=>$blog,'blogs'=>$blogs]);
     }
 }
--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -8,7 +8,7 @@ use App\Testimonial;
 class HomeController extends Controller
 {
     public function index(){
-        $posts=Post::OrderBy('id','DESC')->limit(4)->get();
+        $posts=Post::where('status','Published')->OrderBy('id','DESC')->limit(4)->get();
         $testimonials=Testimonial::all();
         return view('front.home',['posts'=>$posts,'testimonials'=>$testimonials]);
     }
